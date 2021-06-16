658244c2b8e8c68c1ee64acedf924e0a0b8dc9b0
antskos@gmail.com <antskos@gmail.com>
2021-06-16 12:23:34 +0300

    Создание пустого проекта для обзора средств многопоточного программирования

--- Multithreading_treads/Program.cs
+++ Multithreading_treads/Program.cs
@@ -1,10 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
-namespace Multithreading_treads
+
+
+namespace Multithreading_threads
 {
     class Program
     {
